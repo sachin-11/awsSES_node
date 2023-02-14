5f9b45afeb28f6d6d1a06eb336bc4ebb5cf5d8f5
sachin <sachin.singh@manektech.com>
2023-02-14 17:38:25 +0530

    init

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const sendEmail = require('./ses')
 const app = express();
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 app.get('/send-email', (req, res) => {
